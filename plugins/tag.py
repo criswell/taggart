d2780c79bf450a36af66b34ea71f01baf1acfba0
Sam Hart <sam@glencoesoftware.com>
2013-07-24 15:23:34 -0400

    Determine line length, re-structure file

--- plugins/tag.py
+++ plugins/tag.py
@@ -11,6 +11,7 @@
 """
 
 import platform
+import subprocess
 
 import omero
 from omero.cli import BaseControl, CLI, ExceptionHandler
@@ -26,39 +27,17 @@ Examples:
 
 """
 
-def pagetext(text_lined, num_lines=25):
-    for index,line in enumerate(text_lined):
-        if index % num_lines == 0 and index:
-            input=raw_input("Hit any key to continue press q to quit")
-            if input.lower() == 'q':
-                break
-            else:
-                print line
-
-def determine_pagination():
-    """
-    Will attempt to determine console length based upon the current platform.
-
-    Returns the pagination length.
-    """
-    lines = 25 # The default if we can't figure it out
-
-    this_system = platform.system().lower()
-
-    try:
-        if this_system in ['linux', 'darwin', 'macosx', 'cygwin']:
-            #
-        elif this_system in ['windows', 'win32']:
-            #
-    except:
-        # Possible evil to ignore what the error was, but, truthfully,
-        # the reason we do so is because it means it's a platform we
-        # don't have support for, or a platform we should have support
-        # for but which has some non-standard witch-craftery going on
-        sys.ctx.out("Could not determine the console length.")
-
-    return lines
-
+def exec_command(cmd):
+    '''
+    given a command, will execute it in the parent environment
+    Returns a list containing the output
+    '''
+    p = subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    output = p.stdout.readlines()
+    p.stdin.close()
+    p.stdout.close()
+    return output
 
 
 class TagControl(BaseControl):
@@ -82,6 +61,52 @@ class TagControl(BaseControl):
             help="Perform action as an administrator")
         parser.add_argument("--nopage", action="store_true", default=False, \
             help="Disable pagination")
+
+    def pagetext(self, text_lined, num_lines=25):
+        for index,line in enumerate(text_lined):
+            if index % num_lines == 0 and index:
+                input=raw_input("Hit any key to continue press q to quit")
+                if input.lower() == 'q':
+                    break
+                else:
+                    print line
+
+    def determine_pagination():
+        """
+        Will attempt to determine console length based upon the current platform.
+
+        Returns the pagination length.
+        """
+        lines = 25 # The default if we can't figure it out
+
+        this_system = platform.system().lower()
+
+        try:
+            if this_system in ['linux', 'darwin', 'macosx', 'cygwin']:
+                output = exec_command('tput', 'lines'])
+                lines = int(output[0]) if len(output) > 0
+            elif this_system in ['windows', 'win32']:
+                # http://stackoverflow.com/questions/566746/how-to-get-console-window-width-in-python
+                from ctypes import windll, create_string_buffer
+                # stdin handle is -10
+                # stdout handle is -11
+                # stderr handle is -12
+                h = windll.kernel32.GetStdHandle(-12)
+                csbi = create_string_buffer(22)
+                res = windll.kernel32.GetConsoleScreenBufferInfo(h, csbi)
+                if res:
+                    (bufx, bufy, curx, cury, wattr,
+                     left, top, right, bottom,
+                     maxx, maxy) = struct.unpack("hhhhHhhhhhh", csbi.raw)
+                    lines = bottom - top + 1
+        except:
+            # Possible evil to ignore what the error was, but, truthfully,
+            # the reason we do so is because it means it's a platform we
+            # don't have support for, or a platform we should have support
+            # for but which has some non-standard witch-craftery going on
+            sys.ctx.out("Could not determine the console length.")
+
+        return lines
 
     def list_groups(self, args):
         params = omero.sys.ParametersI()
